3f84acff78f1a30ebd664bc8742db158385f0553
AidenMGaming <91638174+AidenMGaming@users.noreply.github.com>
2022-10-28 14:29:44 -0400

    Added chassis Speeds
    Still changing stuff around cuz wpilib doesnt tell me where things go bruh :(

--- src/main/java/frc/robot/subsystems/SwerveDriveKinematics.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveKinematics.java
@@ -6,24 +6,19 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveDriveKinematics {
+    public SwerveDriveKinematics(Translation2d... wheelsMeters) {
+    // Locations for the swerve drive modules relative to the robot center.
+        Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
+        Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
+        Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
+        Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
 
-    public SwerveDriveKinematics(Translation2d... wheelsMeters) {};
-
-    public SwerveModuleState[] toSwerveModuleStates(ChassisSpeeds speeds, Translation2d centerOfRotationMeters) {
-        // Locations for the swerve drive modules relative to the robot center.
-    Translation2d m_frontLeftLocation = new Translation2d(0, 0);
-    Translation2d m_frontRightLocation = new Translation2d(0, -0);
-    Translation2d m_backLeftLocation = new Translation2d(-0, 0);
-    Translation2d m_backRightLocation = new Translation2d(-0, -0);
-
-    // Creating my kinematics object using the module locations
-    SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics (m_frontLeftLocation, 
-        m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
-        return null;
+        // Creating my kinematics object using the module locations
+        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+        m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+        );
     }
-
-    
-    public ChassisSpeeds toChassisSpeeds(SwerveModuleState... wheelStates) {
+    public ChassisSpeeds toChassisSpeeds​(SwerveModuleState... wheelStates) {
         // Example module states
         var frontLeftState = new SwerveModuleState(23.43, Rotation2d.fromDegrees(-140.19));
         var frontRightState = new SwerveModuleState(23.43, Rotation2d.fromDegrees(-39.81));
@@ -39,4 +34,36 @@ public class SwerveDriveKinematics {
         double sideways = chassisSpeeds.vyMetersPerSecond;
         double angular = chassisSpeeds.omegaRadiansPerSecond;
     }
+    public SwerveModuleState[] toSwerveModuleStates​(ChassisSpeeds chassisSpeeds,
+        Translation2d centerOfRotationMeters) {
+            // Example chassis speeds: 1 meter per second forward, 3 meters
+            // per second to the left, and rotation at 1.5 radians per second
+            // counterclockwise.
+        ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
+
+            // Convert to module states
+        SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
+
+            // Front left module state
+        SwerveModuleState frontLeft = moduleStates[0];
+
+            // Front right module state
+        SwerveModuleState frontRight = moduleStates[1];
+
+            // Back left module state
+        SwerveModuleState backLeft = moduleStates[2];
+
+            // Back right module state
+        SwerveModuleState backRight = moduleStates[3];
+    }
+    // The desired field relative speed here is 2 meters per second
+    // toward the opponent's alliance station wall, and 2 meters per
+    // second toward the left field boundary. The desired rotation
+    // is a quarter of a rotation per second counterclockwise. The current
+    // robot angle is 45 degrees.
+    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+    2.0, 2.0, Math.PI / 2.0, Rotation2d.fromDegrees(45.0));
+  
+    // Now use this in our kinematics
+    SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
 }
--- src/main/java/frc/robot/subsystems/SwerveModuleState.java
+++ src/main/java/frc/robot/subsystems/SwerveModuleState.java
@@ -5,33 +5,15 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.subsystems.SwerveDriveKinematics;
 
 public class SwerveModuleState {
-    
-    public SwerveModuleState[] toSwerveModuleStates(ChassisSpeeds speeds) {
-        // Example chassis speeds: 1 meter per second forward, 3 meters
-        // per second to the left, and rotation at 1.5 radians per second
-        // counterclockwise.
-        ChassisSpeeds speed = new ChassisSpeeds(1.0, 3.0, 1.5);
-    
-        // Convert to module states
-        SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
-    
-        // Front left module state
-        SwerveModuleState frontLeft = moduleStates[0];
-    
-        // Front right module state
-        SwerveModuleState frontRight = moduleStates[1];
-    
-        // Back left module state
-        SwerveModuleState backLeft = moduleStates[2];
-    
-        // Back right module state
-        SwerveModuleState backRight = moduleStates[3];
+    public int compareTo​(SwerveModuleState other) {
         
-        }
-
-    private static Object optimize(SwerveModuleState swerveModuleState, Rotation2d rotation2d) {
+    }
+    public String toString(){
+        
+    }
+    public static SwerveModuleState optimize​(SwerveModuleState desiredState,
+Rotation2d currentAngle) {
         var frontLeftOptimized = SwerveModuleState.optimize(frontLeft,
             new Rotation2d(m_turningEncoder.getDistance()));
-        return null;
     }
 }
